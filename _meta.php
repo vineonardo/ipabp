42671f01394955216dd051a9af18b8ce8e5b5ae7
Vineet <vineonardo@gmail.com>
2019-11-25 18:24:21 +0530

    Google Font & Font Icon added

---
+++ _meta.php
@@ -0,0 +1,8 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="stylesheet" href="css/custom.min.css">
+<link rel="icon" type="image/x-icon" href="https://static.abplive.com/frontend/abplive/images/favicon.ico">
+<link rel="shortcut icon" type="image/x-icon" href="https://static.abplive.com/frontend/abplive/images/favicon.ico">
+<link href="https://fonts.googleapis.com/css?family=Cambay:400,700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="css/override.css" />
+<!-- post development, override.css's content should be placed in Head tag directly with minification -->
