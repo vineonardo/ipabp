f637a13585b12ba7c2a4e405997b1452c005dcf2
Vineet <vineonardo@gmail.com>
2019-11-29 17:53:35 +0530

    meta update

--- _meta.php
+++ _meta.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1" />
-<link rel="stylesheet" href="css/custom.min.css">
+<link rel="stylesheet" href="css/custom.min.css?v=<?=time()?>">
 <link rel="icon" type="image/x-icon" href="https://static.abplive.com/frontend/abplive/images/favicon.ico">
 <link rel="shortcut icon" type="image/x-icon" href="https://static.abplive.com/frontend/abplive/images/favicon.ico">
 <link href="https://fonts.googleapis.com/css?family=Cambay:400,700&display=swap" rel="stylesheet">
