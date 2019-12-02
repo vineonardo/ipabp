aae8fb9247f29abdf17e94a8a060175851bcd176
Vineet <vineonardo@gmail.com>
2019-12-02 19:41:34 +0530

    Font size fixes + Added Cambay font

---
+++ desktop/_newsListItem.php
@@ -0,0 +1,10 @@
+<a href="#" title="पहलवान बबीता फोगाट का सब इंस्पेक्टर पद से दिया इस्तीफा मंजूर, चुनाव लड़ने की अटकलें">
+	<div class="uk-grid-collapse" uk-grid>
+		<div class="uk-width-2-3 fz20 p-10">
+			पहलवान बबीता फोगाट का सब इंस्पेक्टर पद से दिया इस्तीफा मंजूर, चुनाव लड़ने की अटकलें
+		</div>
+		<div class="uk-width-1-3 uk-position-relative uk-padding-remove-left">
+			<img src="images/article1.png" alt="image.name" />
+		</div>
+	</div>
+</a>
